8b661e0e72fa8a4f8f49477d368451ad9b15c108
Kennedy <meetkenn2diol@gmail.com>
2026-03-26 14:05:24 +0100

    1. Updated checkoutpage for dynamic filling.
    2. created a function to return money in dollars

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { formatMoney } from "../utils/Money";
 import "./home-page.css";
 
 function HomePage({cart}) {
@@ -45,7 +46,7 @@ function HomePage({cart}) {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  {formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
