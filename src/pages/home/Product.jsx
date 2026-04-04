43359acc06c68b684a0ab9d38f54c40236eb6ddc
Kennedy <meetkenn2diol@gmail.com>
2026-04-04 23:33:22 +0100

    feat: implement integrated tests for HomePage rendering

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -24,7 +24,7 @@ export default function Product({ product, loadCart }) {
   };
 
   return (
-    <div className="product-container">
+    <div className="product-container" data-testid="product-container">
       <div className="product-image-container">
         <img
           className="product-image"
